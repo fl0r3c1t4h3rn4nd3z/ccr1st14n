beedc2dc2e124096e8b15dcb9ac37be49b4b9ad7
Ander <npander@hotmail.com>
2018-01-16 16:32:45 -0500

    Configuracion del controlador prinicpal welcome

---
+++ application/views/a2z/footer.php
@@ -0,0 +1,68 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<!--//footer  -->
+	<!-- js -->
+	<script src="js/jquery-2.2.3.min.js"></script>
+	<!-- //js-->
+	<!--banner-slider-->
+	<script src="js/JiSlider.js"></script>
+	<script> 
+		$(window).load(function () {
+			$('#JiSlider').JiSlider({
+				color: '#fff',
+				start: 1,
+				reverse: false
+			}).addClass('ff')
+		})
+	</script>
+	<!-- //banner-slider -->
+	<!--search-bar-->
+	<script src="js/main.js"></script>
+	<!--//search-bar-->
+	<!-- start-smooth-scrolling -->
+	<script  src="js/move-top.js"></script>
+	<script  src="js/easing.js"></script>
+	<script> 
+		jQuery(document).ready(function ($) {
+			$(".scroll").click(function (event) {
+				event.preventDefault();
+
+				$('html,body').animate({
+					scrollTop: $(this.hash).offset().top
+				}, 1000);
+			});
+		});
+	</script>
+	<!-- //end-smooth-scrolling -->
+	<!-- smooth-scrolling-of-move-up -->
+	<script> 
+		$(document).ready(function () {
+			/*
+			var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+			};
+			*/
+
+			$().UItoTop({
+				easingType: 'easeOutQuart'
+			});
+
+		});
+	</script>
+	<script  src="js/SmoothScroll.min.js"></script>
+	<!-- Bootstrap core JavaScript
+    ================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="js/bootstrap.js"></script>
+</body>
+
+</html>
---
+++ application/views/a2z/header.php
@@ -0,0 +1,137 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="zxx">
+
+    <head>
+        <title>A2Z a Corporate Category Flat Bootstrap Responsive Website Template | Home :: w3layouts</title>
+        <!-- for-mobile-apps -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="keywords" content="a2z Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+              Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+        <script>
+            addEventListener("load", function () {
+                setTimeout(hideURLbar, 0);
+            }, false);
+
+            function hideURLbar() {
+                window.scrollTo(0, 1);
+            }
+        </script>
+        <!-- //for-mobile-apps -->
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+        <!--banner slider  -->
+        <link href="css/JiSlider.css" rel="stylesheet">
+        <!-- //banner-slider -->
+        <link href="css/font-awesome.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="//fonts.googleapis.com/css?family=Noto+Serif:400,400i,700,700i" rel="stylesheet">
+        <link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:200,200i,300,300i,400,400i,600,600i,700,700i,900,900i" rel="stylesheet">
+
+    </head>
+
+    <body>
+        <!-- header -->
+        <div class="w3layouts-header">
+            <div class="container">
+
+                <div class="logo-nav-agileits">
+                    <div class="logo-nav-left">
+                        <h1>
+                            <a href="index.html">
+                                <span class="fa fa-home"></span>a2z
+                                <span class="logo-title">home services</span>
+                            </a>
+                        </h1>
+                    </div>
+
+                    <div class="header-grid-left-wthree">
+                        <div class="h3-title">
+                            <h3>contact us</h3>
+                        </div>
+                        <ul>
+                            <li>
+                                <span class="fa fa-envelope" aria-hidden="true"></span>
+                                <a href="mailto:info@example.com">a2zservices.com</a>
+                            </li>
+                            <li>
+                                <span class="fa fa-phone" aria-hidden="true"></span>+1234 567 892</li>
+                            <li>
+                                <span class="fa fa-mobile" aria-hidden="true"></span>+12 234 5678
+                            </li>
+                        </ul>
+                        <div class="clearfix"> </div>
+                    </div>
+                    <div class="clearfix"> </div>
+                </div>
+                <div class="clearfix"> </div>
+                <div class="logo-nav-left1">
+                    <nav class="navbar navbar-default">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed navbar-toggle1" data-toggle="collapse" data-target="#bs-megadropdown-tabs">Menu
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                        </div>
+
+                        <div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
+                            <ul class="nav navbar-nav">
+                                <li class="active">
+                                    <a href="index.html">Home</a>
+                                </li>
+                                <li>
+                                    <a href="about.html">about us</a>
+                                </li>
+                                <li>
+                                    <a href="services.html">services</a>
+                                </li>
+                                <li>
+                                    <a href="projects.html">Projects</a>
+                                </li>
+                                <li>
+                                    <a href="plan.html">plans</a>
+                                </li>
+                                <li>
+                                    <a href="contact.html">contact us</a>
+                                </li>
+                                <li>
+                                    <a href="status.html">track service status</a>
+                                </li>
+                                <li class="s-bar">
+                                    <div class="search-w3_agileits">
+                                        <input class="search_box" type="checkbox" id="search_box">
+                                        <label class="icon-search" for="search_box">
+                                            <span class="fa fa-search" aria-hidden="true"></span>
+                                        </label>
+                                        <div class="search_form">
+                                            <form action="#" method="post">
+                                                <input type="search" name="Search" placeholder=" " />
+                                                <input type="submit" value="Search">
+                                            </form>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </nav>
+                </div>
+                <div class="clearfix"> </div>
+            </div>
+        </div>
+        <!-- //header -->
